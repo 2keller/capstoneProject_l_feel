1ee195b979870b64f97e052669c10a803e490bdb
2keller <mashayamombekelvin@gmail.com>
2025-10-11 12:07:13 +0200

    fixed the models and the signup page

--- core/forms.py
+++ core/forms.py
@@ -8,24 +8,11 @@ class UserRegistration(UserCreationForm):
     name = forms.CharField(max_length=20)
     surname = forms.CharField(max_length=20)
 
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['name', 'surname', 'bio']
-
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField(required=True)
-    name = forms.CharField(max_length=20)
-    surname = forms.CharField(max_length=20)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'name', 'surname']
-
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        fields = ['username', 'password']
-        
--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path 
 from .views import SignUpView, feed
 from  django.contrib.auth import views
-
+from .views import SignUpView
 
 urlpatterns = [
 
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('feed/', feed, name='feed'), 
-  
+    path('feed/', feed, name='feed'),
+    path ('login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('signup/', SignUpView.as_view(template_name='registration/signup.html'), name='signup'),
 ]
+
+
